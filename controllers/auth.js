1b1f6b17a57ad51cd77f64a772f28a4a623af7f7
unknown <dionisiusst2@gmail.com>
2021-02-07 20:40:28 +0700

    Add auth middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,8 +6,14 @@ const config = require('config');
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-exports.getLoggedInUser = (req, res) => {
-  res.send('Get logged in user');
+exports.getLoggedInUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 };
 
 // @route     POST api/auth
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getLoggedInUser, login } = require('../controllers/auth');
 const { logInValidationRules, validate } = require('../validators/user');
+const auth = require('../middleware/auth');
 
 router
   .route('/')
-  .get(getLoggedInUser)
+  .get(auth, getLoggedInUser)
   .post(logInValidationRules(), validate, login);
 
 module.exports = router;
